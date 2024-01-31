ea1a02da8819a2f59efd00113eb19200b1e4ba17
Tiger <tagir.tsarkov@gmail.com>
2024-01-31 18:12:40 +0200

    s

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function Page({lang}) {
                   <img className="app-image" src={iFolder+"/topic6/arp2.png"} alt=''></img>
                   <img className="app-image app-pos1" src={iFolder+"/topic6/arp.png"} alt=''></img>
               </div>
-              <TextBlock lang={lang} theme="topic" title={lang.topic6Title} text={lang.topic6Text} list={lang.topic6List} link={"https://github.com/tagir215/AfricanRockPythonServer"} link2={"https://github.com/tagir215/african-rock-python-client"} 
+              <TextBlock lang={lang} theme="topic" title={lang.topic6Title} text={lang.topic6Text} list={lang.topic6List} link={"https://github.com/tagir215/african-rock-python-server"} link2={"https://github.com/tagir215/arp-client"} 
 	  //link3={"https://arp-client-c1df0d4c8a26.herokuapp.com"}
 	  />
             </div>
